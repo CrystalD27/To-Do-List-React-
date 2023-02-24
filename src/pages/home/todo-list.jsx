5ab5e12082714c2871df6ab3454b2bb41f5a0a4d
Crystal <ytgliniak@gmail.com>
2023-02-24 16:23:45 +0100

    refactor

--- src/pages/home/todo-list.jsx
+++ src/pages/home/todo-list.jsx
@@ -1,35 +1,18 @@
-const getToDoList = () => {
-    return (
-        <>
-            <ul>
-                {items.map((item) => {
-                    return (
-                        <li key={item.id} className="border-b">
-                            <label className="todoList_label relative flex items-center">
-                                <input
-                                    type="checkbox"
-                                    value="completed"
-                                    className="relative h-6 w-6 rounded-full shadow"
-                                />
+import { ToDo } from './todo';
+export const ToDoList = (props) => {
+    const { todoList, getTodoList } = props;
 
-                                <div className="flex items-center  py-4 pl-4 ">
-                                    <div
-                                        className="flex items-center  hover:text-black"
-                                        key={item.id}
-                                    ></div>{' '}
-                                    <button>
-                                        {' '}
-                                        <CgClose className="absolute right-[-2rem] bottom-4 text-xl  text-white  hover:text-black" />
-                                        {item.value}
-                                    </button>
-                                </div>
-                            </label>
-                        </li>
-                    );
-                })}
-            </ul>
-        </>
+    return (
+        <ul>
+            {todoList.map((todo) => (
+                <ToDo
+                    key={todo.id}
+                    id={todo.id}
+                    completedAt={todo.completed_at}
+                    content={todo.content}
+                    getTodoList={getTodoList}
+                />
+            ))}
+        </ul>
     );
 };
-
-export default getToDoList;
