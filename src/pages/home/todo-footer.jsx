7ca4a188cc7ccb6be37757b0b0c22326021c5297
Crystal <ytgliniak@gmail.com>
2023-02-24 21:27:37 +0100

    feat: add clear all completed function

--- src/pages/home/todo-footer.jsx
+++ src/pages/home/todo-footer.jsx
@@ -3,15 +3,22 @@ import Swal from 'sweetalert2';
 export const ToDoFooter = (props) => {
     const { getTodoList, todoListState } = props;
     const { deleteToDo } = useDeleteToDo();
+    const activeTodoCount =
+        todoListState.data?.todos?.filter((item) => !item.completed_at)?.length ?? 0;
+    const activeTodoText = activeTodoCount <= 1 ? 'active item' : 'active items';
     const deleteAllHandler = async () => {
         try {
-            const todoList = props.todoListState.data.todos;
-            todoList.forEach(async (todo) => {
+            const completedTodos = todoListState.data?.todos?.filter((todo) => todo.completed_at);
+            completedTodos.forEach(async (todo) => {
                 await deleteToDo(todo.id);
             });
-
             await getTodoList();
-            Swal.fire('All items have been deleted');
+            console.log(completedTodos);
+            if (completedTodos.length === 0) {
+                Swal.fire('There is no complted item');
+            } else {
+                Swal.fire('Completed items have been deleted');
+            }
         } catch (error) {
             console.error(error);
         }
@@ -20,13 +27,11 @@ export const ToDoFooter = (props) => {
     return (
         <div className="footerBox flex justify-between pl-5 pt-6">
             <p>
-                <span className="mr-2">
-                    {todoListState.data?.todos?.filter((item) => !item.completed_at)?.length ?? 0}
-                </span>
-                <span>active item</span>
+                <span className="mr-2">{activeTodoCount}</span>
+                <span>{activeTodoText}</span>
             </p>
-            <button type="button" onClick={deleteAllHandler}>
-                clear completed item
+            <button className="text-gray-400" type="button" onClick={deleteAllHandler}>
+                clear completed items
             </button>
         </div>
     );
